d6c5e1a99004d60934d6f254e36a951764449b9a
defr0stt <dima.sb713@gmail.com>
2022-01-30 17:39:22 +0200

    Properties added like set()

--- src/main/java/ua/lpnu/spring/music/MusicPlayer.java
+++ src/main/java/ua/lpnu/spring/music/MusicPlayer.java
@@ -3,10 +3,30 @@ package ua.lpnu.spring.music;
 public class MusicPlayer
 {
     private Music music;
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     public MusicPlayer(Music music) {
         this.music = music;
     }
+
+    public MusicPlayer(){}
 
     public String getMusic() {
         return music.playMusic();
--- src/main/java/ua/lpnu/spring/music/TestMusic.java
+++ src/main/java/ua/lpnu/spring/music/TestMusic.java
@@ -9,10 +9,14 @@ public class TestMusic
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music music = context.getBean("music", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        Music music = context.getBean("music", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         System.out.println("\n > " + musicPlayer.getMusic());
+        System.out.println("Name : " + musicPlayer.getName());
+        System.out.println("Volume : " + musicPlayer.getVolume());
 
         context.close();
     }
